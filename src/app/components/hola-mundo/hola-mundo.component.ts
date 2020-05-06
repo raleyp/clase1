81e1ea3f59978dfeecd8d91f3ad2aabd6ab8527a
raleyp <61215140+raleyp@users.noreply.github.com>
2020-05-05 20:12:02 -0600

    Roberto
    Curso Angular

---
+++ src/app/components/hola-mundo/hola-mundo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hola-mundo',
+  templateUrl: './hola-mundo.component.html',
+  styleUrls: ['./hola-mundo.component.scss']
+})
+export class HolaMundoComponent implements OnInit {
+
+  public miNombre = 'Roberto Aley'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
